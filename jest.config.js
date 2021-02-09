cf4f511655fa3ff94efe40350a7e54a3207a6894
Jessica Sachs <jess@jessicasachs.io>
2021-02-08 22:41:44 -0500

    clean vue-router-mock example

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
     '^.+\\.vue$': 'vue-jest'
   }
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,8 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
-
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
+import { mount } from '@vue/test-utils'
+describe('Example', () => {
+  it('basic component', () => {
+    mount({
+      template: `<div></div>`
     })
-    expect(wrapper.text()).toMatch(msg)
   })
 })
